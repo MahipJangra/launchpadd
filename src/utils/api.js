09bd9365385400f3c9ec3c6fbf62f2afe733e173
MahipJangra <mahipjangra8@gmail.com>
2026-06-11 00:29:58 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:4000/api" });
+const api = axios.create({ baseURL: "https://launchpad-frontend-vhhb.onrender.com" });
 
 export async function analyzeIdea(messages) {
   const { data } = await api.post("/analyze", { messages });
